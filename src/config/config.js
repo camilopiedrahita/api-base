46d545c14a5b9ef44400ef5fbeaef6012dab5981
Camilo Piedrahita Hernández <cpiedrahitah@gmail.com>
2021-04-17 17:55:37 -0500

    Fixed server port v2

--- src/config/config.js
+++ src/config/config.js
@@ -6,8 +6,6 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const envVarsSchema = Joi.object()
   .keys({
-    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    PORT: Joi.number().default(3000),
     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
@@ -33,8 +31,6 @@ if (error) {
 }
 
 module.exports = {
-  env: envVars.NODE_ENV,
-  port: envVars.PORT,
   mongoose: {
     url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
     options: {
